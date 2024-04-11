2675e7178059795054740cc196959dda085d5734
Victor Ekeleme <74677661+victorekeleme@users.noreply.github.com>
2024-04-11 10:56:34 -0400

    Update generalize.bash

--- generalize.bash
+++ generalize.bash
@@ -38,7 +38,7 @@ nmcli conn migrate
 
 # Step 5
 sudo ln -s /dev/null /etc/udev/rules.d/75-persistent-net-generator.rules
-sudo rm -f /etc/udev/rules.d/70-persistent-net.rules
+# sudo rm -f /etc/udev/rules.d/70-persistent-net.rules
 
 # Step 7
 sudo dnf -y update
@@ -73,12 +73,28 @@ sudo sed -i '/ - disk_setup/d' /etc/cloud/cloud.cfg
 sudo sed -i '/cloud_init_modules/a\\ - mounts' /etc/cloud/cloud.cfg
 sudo sed -i '/cloud_init_modules/a\\ - disk_setup' /etc/cloud/cloud.cfg
 
-# Create azure datasource config file
-sudo tee /etc/cloud/cloud.cfg.d/91-azure_datasource.cfg <<EOF
+echo "Allow only Azure datasource, disable fetching network setting via IMDS"
+cat << 'EOF' > /etc/cloud/cloud.cfg.d/91-azure_datasource.cfg
 datasource_list: [ Azure ]
 datasource:
     Azure:
         apply_network_config: False
+
+EOF
+
+if [[ -f /mnt/resource/swapfile ]]; then
+echo Removing swapfile - RHEL uses a swapfile by default
+swapoff /mnt/resource/swapfile
+rm /mnt/resource/swapfile -f
+fi
+
+echo "Add console log file"
+cat << 'EOF' >> /etc/cloud/cloud.cfg.d/05_logging.cfg
+## This tells cloud-init to redirect its stdout and stderr to
+## 'tee -a /var/log/cloud-init-output.log' so the user can see output
+## there without needing to look on the console.
+output: {all: '| tee -a /var/log/cloud-init-output.log'}
+
 EOF
 
 
@@ -87,7 +103,8 @@ EOF
 # sudo cloud-init clean
 # sudo waagent -force -deprovision+user
 # sudo rm -f ~/.bash_history
-# sudo export HISTSIZE=0
+# export HISTSIZE=0
+# history -c
 
 # Shutdown the system
 # systemctl poweroff
